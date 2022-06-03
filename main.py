fd07bf203a3e16e7e2e88872ff9e6bc72a60ad9f
b10t <b10t.rus@gmail.com>
2022-06-03 22:36:48 +0800

    Using long polling.

--- main.py
+++ main.py
@@ -8,13 +8,25 @@ if __name__ == '__main__':
 
     devman_token = env('DEVMAN_TOKEN', 'DEVMAN_TOKEN')
 
-    user_reviews_url = 'https://dvmn.org/api/user_reviews/'
+    user_reviews_url = 'https://dvmn.org/api/long_polling/'
 
     headers = {
         'Authorization': f'Token {devman_token}',
     }
 
-    response = requests.get(user_reviews_url, headers=headers)
-    response.raise_for_status()
+    params = {}
 
-    print(response.text)
+    while True:
+        response = requests.get(
+            user_reviews_url,
+            headers=headers,
+            params=params
+        )
+        response.raise_for_status()
+
+        resp_json = response.json()
+
+        print(resp_json)
+
+        if 'status' in resp_json and resp_json['status'] == 'timeout':
+            params.update(timestamp=resp_json.get('timestamp_to_request'))
