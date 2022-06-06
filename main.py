1f36d8cc34daf32752f881598dc2bd78f8f539f1
b10t <b10t.rus@gmail.com>
2022-06-06 15:53:34 +0800

    Dried the code.

--- main.py
+++ main.py
@@ -9,34 +9,9 @@ from telegram.ext import (CallbackContext, CommandHandler, Filters,
 def create_and_start_bot(telegram_token):
     """Creates and launches a telegram bot."""
     updater = Updater(telegram_token)
-
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
-
     updater.start_polling()
 
     return updater.bot
-
-
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_text(f'Hi {user.first_name}!')
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
 
 
 if __name__ == '__main__':
