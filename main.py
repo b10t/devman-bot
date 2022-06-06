2eed74392090f9d435b2f7d1009054d722ba063d
b10t <b10t.rus@gmail.com>
2022-06-06 16:16:35 +0800

    We use a multiline string.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import requests
 import time
+from textwrap import dedent
+
+import requests
 from environs import Env
-from telegram import Update
-from telegram.ext import (CallbackContext, CommandHandler, Filters,
-                          MessageHandler, Updater)
+from telegram.ext import Updater
 
 
 def create_and_start_bot(telegram_token):
@@ -59,13 +59,16 @@ if __name__ == '__main__':
                 if review.get('is_negative'):
                     review_status = 'К сожалению, в работе нашлись ошибки.'
 
-                telegram_bot_message = f'У Вас проверили работу "{review.get("lesson_title")}".\n\n' \
-                    f'{review_status}\n\n' \
-                    f'Ссылка на урок: {review.get("lesson_url")}'
+                telegram_bot_message = f'''\
+                    У Вас проверили работу "{review.get("lesson_title")}".
+
+                    {review_status}
+
+                    Ссылка на урок: {review.get("lesson_url")}'''
 
                 telegram_bot.send_message(
                     chat_id=telegram_chat_id,
-                    text=telegram_bot_message
+                    text=dedent(telegram_bot_message)
                 )
 
         params.update(timestamp=timestamp)
