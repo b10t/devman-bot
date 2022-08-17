4e29b6fc2d3a901728c610b6b87690160ac71ae5
b10t <b10t.rus@gmail.com>
2022-08-17 08:50:47 +0700

    Added command 'report'.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import datetime
 import logging
 import time
 import traceback
 from textwrap import dedent
 
+import pytz
 import requests
-import telegram
 from environs import Env
+from memory_profiler import memory_usage
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler, Dispatcher, Updater
+from telegram.utils.helpers import escape_markdown
 
-logger = logging.getLogger("TelegramBotLogger")
+logger = logging.getLogger('TelegramBotLogger')
+
+start_time = time.time()
 
 
 class BotLogsHandler(logging.Handler):
@@ -25,9 +32,39 @@ class BotLogsHandler(logging.Handler):
         )
 
 
+def get_memory_usage():
+    """Возвращает кол-во используемой памяти."""
+    memory = round(memory_usage().pop(), 3)
+    return escape_markdown(
+        f'{memory}',
+        2
+    )
+
+
+def report(update: Update, context: CallbackContext) -> None:
+    """Отчёт об текущем состоянии бота."""
+    message_text = dedent(
+        f'''
+            *Текущий статус:*
+            RAM:   `{get_memory_usage()} MiB`
+            Start: `{time.ctime(start_time)}`
+            UPT:   `{str(datetime.timedelta(seconds=(time.time() - start_time)))}`
+        '''
+    )
+
+    # `{datetime.datetime.fromtimestamp(time.time() - start_time, pytz.utc)}`
+
+    update.message.reply_text(
+        message_text,
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
+
+
 if __name__ == '__main__':
     logging.basicConfig(format="%(message)s")
     logger.setLevel(logging.INFO)
+
+    print(memory_usage())
 
     env = Env()
     env.read_env()
@@ -36,10 +73,16 @@ if __name__ == '__main__':
     telegram_chat_id = env.int('TELEGRAM_CHAT_ID', 0)
     devman_token = env('DEVMAN_TOKEN', 'DEVMAN_TOKEN')
 
-    telegram_bot = telegram.Bot(telegram_token)
+    updater = Updater(telegram_token)
+    dispatcher = updater.dispatcher
+    telegram_bot = dispatcher.bot
+
+    dispatcher.add_handler(CommandHandler('report', report))
 
     bot_logs_handler = BotLogsHandler(telegram_bot, telegram_chat_id)
     logger.addHandler(bot_logs_handler)
+
+    updater.start_polling()
 
     user_reviews_url = 'https://dvmn.org/api/long_polling/'
 
